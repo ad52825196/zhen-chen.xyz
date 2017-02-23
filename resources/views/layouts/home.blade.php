a2320b633db0a9c805a3491ffac70a6037211cfb
Zhen Chen <me@zhen-chen.com>
2017-02-24 12:30:28 +1300

    Basic setup

---
+++ resources/views/layouts/home.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="author" content="Zhen Chen" />
+    <meta name="keywords" content="{{ $keywords }}" />
+
+    <title>{{ $title }}</title>
+
+    <link rel="canonical" href="{{ $canonical }}" />
+
+    <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+
+    <link rel="stylesheet" href="/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="/css/bootstrap-theme.min.css" />
+    <link rel="stylesheet" href="/css/font-awesome.min.css" />
+    <link rel="stylesheet" href="/css/weather-icons.min.css" />
+    <link rel="stylesheet" href="/css/weather-icons-wind.min.css" />
+    <link rel="stylesheet" href="/css/main.css" />
+
+    @yield('head')
+</head>
+<body>
+    @include('analytics')
+</body>
+</html>
