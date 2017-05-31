1802f2a813fad3cbee963137823e9695f9bea0a3
Zhen Chen <me@zhen-chen.com>
2017-06-01 11:50:25 +1200

    Sync main.js with .com

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,11 @@
-// add target attribute to links pointing to other sites
-$(document.links).filter(function() {
-    return this.hostname != window.location.hostname;
-}).attr('target', '_blank');
-
-// leave space for footer
+// leave space for header and footer
+$('body')[0].style.paddingTop = $('nav').outerHeight(false) + 'px';
 $('body')[0].style.paddingBottom = $('footer').outerHeight(true) + 'px';
+
+// add target attribute to links pointing to other sites
+function linkTarget() {
+    $(document.links).filter(function() {
+        return this.hostname != window.location.hostname;
+    }).attr('target', '_blank');
+}
+$(linkTarget);
